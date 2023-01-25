62caadc36b6dd4ced1a1f40f7938f0560ffbdc1a
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-25 09:47:47 +0100

    gatsby-image plugins configured

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `gatsby-app`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ],
 }
